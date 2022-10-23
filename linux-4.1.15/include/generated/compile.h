d2112d80772453d8dabfafcdde0f8140d97f24b4
Charlie-Ni <ncl_future@163.com>
2022-10-23 01:27:22 -0700

    base kernel

---
+++ linux-4.1.15/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 96 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#96 SMP PREEMPT Sat Apr 30 21:06:52 CST 2022"
+#define LINUX_COMPILE_BY "jason"
+#define LINUX_COMPILE_HOST "jason-VirtualBox"
+#define LINUX_COMPILER "gcc version 4.9.2 20140904 (prerelease) (crosstool-NG linaro-1.13.1-4.9-2014.09 - Linaro GCC 4.9-2014.09) "
